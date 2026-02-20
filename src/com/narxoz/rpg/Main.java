5e89515106b99f067cf3ea7c2e8012c51f5a115a
shayanbayersin-arch <shayanbayersin@gmail.com>
2026-02-20 10:50:48 +0500

    Add the Demo combat battle in Main

--- src/com/narxoz/rpg/Main.java
+++ src/com/narxoz/rpg/Main.java
@@ -1,12 +1,28 @@
 package com.narxoz.rpg;
 
-
+import com.narxoz.rpg.character.Mage;
 
 public class Main {
     public static void main(String[] args) {
-        Goblin goblin = new Goblin("Grimtooth");
-        goblin.displayInfo();
-    
+        
+        Mage hero = new Mage("Gandalf");
+        Goblin goblin = new Goblin("Grub");
 
+
+        System.out.println("Battle Start!");
+
+        while (hero.isAlive() && goblin.isAlive()) {
+            hero.attack(goblin);
+
+
+            if (goblin.isAlive()) {
+                goblin.attack(hero);
+            }
+
+
+        }
+        System.out.println("Battle End!");
+
+    
     }
 }
